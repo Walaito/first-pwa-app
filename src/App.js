e2e826967a697fe011b8fd7df29469d9b247ad56
Walaiporn Tohnual <eaawato@students.eaaa.dk>
2020-02-21 09:22:00 +0100

    Add repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import React, {useState, useEffect} from "react";
+import "./App.css";
+import video from "./videos/Underwater.mp4";
 
 function App() {
+  /*const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(
+        "http://orangevalleycaa.org/api/videos/"
+      ).then(res => res.json());
+      setData(result);
+    };
+    fetchData();
+  }, []);*/
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h2>Duck Art Videos</h2>
       </header>
+
+      {/* {data.map(video => (
+        <div key={video.id}>
+          <h2>{video.name}</h2>
+          <video height={200} controls src={video.video_url}></video>
+        </div>
+      ))} */}
+
+      <video height={200} controls src={video}></video>
     </div>
   );
 }
